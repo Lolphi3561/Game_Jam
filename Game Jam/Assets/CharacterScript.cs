2dd9a88ec4878a50fc3a4c5a5f59902c0d2b1122
Talha Zengin <168358523+Lolphi3561@users.noreply.github.com>
2026-02-02 11:41:53 +0100

    jump

--- Game Jam/Assets/CharacterScript.cs
+++ Game Jam/Assets/CharacterScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class CharacterScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
+    public float jumpPower = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,9 +15,9 @@ public class CharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            myRigidBody.linearVelocity = Vector2.up * 10;
+            myRigidBody.linearVelocity = Vector2.up * jumpPower;
         }
     }
 }
