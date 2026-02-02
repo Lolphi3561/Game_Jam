d761b943f3f2446544a4eb858b889f0c422ddcfd
Talha Zengin <168358523+Lolphi3561@users.noreply.github.com>
2026-02-02 16:58:52 +0100

    wallcling semi added

--- Game Jam/Assets/CharacterScript.cs
+++ Game Jam/Assets/CharacterScript.cs
@@ -5,35 +5,57 @@ using static UnityEngine.LowLevelPhysics2D.PhysicsShape;
 
 public class CharacterScript : MonoBehaviour
 {
+    // Allgemien
     public Rigidbody2D myRigidBody;
-    public float jumpPower = 5;
+    public float jumpPower = 10;
     public int jumpCount = 2;
     public bool isAlive = true;
     public GameObject floorDeathBox;
+    public Collision2D collision;
 
     private bool lastDirectionLeft = true;
+
+    // WallCling
+    public PhysicsMaterial2D NoFriction;
+    public PhysicsMaterial2D WallFriction;
+    private Collider2D col;
+
+
+    // Dash
     public bool hasDash = true;
-    public float dashCooldown = (float)1.25;
+    public float dashCooldown = 0.75f;
     public float dashTimer = 0;
+    public float dashBoost = 20;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        col = GetComponent<Collider2D>();
+        col.sharedMaterial = NoFriction;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+
+        Dash();
+
+        WallCling();
+    }
+
+    private void Movement()
+    {
+        // Totesfall
         if (isAlive == false)
         {
-            transform.position = new Vector3(0,8,1);
+            transform.position = new Vector3(0, 8, 1);
             isAlive = true;
         }
 
         if (Keyboard.current.spaceKey.wasPressedThisFrame && jumpCount >= 1)
         {
-            myRigidBody.linearVelocity = Vector2.up * jumpPower;
+            myRigidBody.linearVelocityY = jumpPower;
             jumpCount -= 1;
         }
 
@@ -48,16 +70,17 @@ public class CharacterScript : MonoBehaviour
         }
         else if (Keyboard.current.dKey.isPressed)
         {
-            if(myRigidBody.linearVelocityX < 5)
+            if (myRigidBody.linearVelocityX < 5)
             {
                 myRigidBody.linearVelocityX = 5;
             }
             lastDirectionLeft = false;
         }
+
         if (lastDirectionLeft && !Keyboard.current.aKey.isPressed || myRigidBody.linearVelocityX < -5)
         {
             myRigidBody.linearVelocityX += (float)0.5;
-            if(myRigidBody.linearVelocityX > 0)
+            if (myRigidBody.linearVelocityX > 0)
             {
                 myRigidBody.linearVelocityX = 0;
             }
@@ -69,14 +92,6 @@ public class CharacterScript : MonoBehaviour
             {
                 myRigidBody.linearVelocityX = 0;
             }
-        }
-        if (Keyboard.current.shiftKey.wasPressedThisFrame)
-        {
-            Dash();
-        }
-        if(dashTimer < dashCooldown)
-        {
-            dashTimer += Time.deltaTime;
         }
     }
 
@@ -95,29 +110,59 @@ public class CharacterScript : MonoBehaviour
         {
             isAlive = false;
         }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            myRigidBody.linearVelocityX = 0;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if(Keyboard.current.spaceKey.isPressed == false)
         {
-            jumpCount -= 1;
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f)
+                {
+                    jumpCount -= 1;
+                    break;
+                }
+            }
         }
     }
 
     private void Dash()
     {
-        if(dashTimer >= dashCooldown)
+        if (dashTimer < dashCooldown)
+        {
+            dashTimer += Time.deltaTime;
+        }
+
+        if (dashTimer >= dashCooldown && Keyboard.current.shiftKey.wasPressedThisFrame)
         {
             if (lastDirectionLeft)
             {
-                myRigidBody.linearVelocityX -= 20;
+                myRigidBody.linearVelocityX -= dashBoost;
             }
             else
             {
-                myRigidBody.linearVelocityX += 20;
+                myRigidBody.linearVelocityX += dashBoost;
             }
             dashTimer = 0;
+        }
+    }
+
+    private void WallCling(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.normal.y == 0f)
+            {
+                col.sharedMaterial = WallFriction;
+                jumpCount = 1;
+                break;
+            }
         }
     }
 }
