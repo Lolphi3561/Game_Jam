55e887b156566e39446d7e509e7f4a9dd5cc920a
FlexFrosch <colin.kueer@gmail.com>
2026-02-04 08:57:56 +0100

    Arrow despawn gefixed

--- Game Jam/Assets/ArrowScript.cs
+++ Game Jam/Assets/ArrowScript.cs
@@ -4,22 +4,17 @@ public class ArrowScript : MonoBehaviour
 {
     public int arrowSpeed = 50;
     private Transform crossbow;
-    private static int direction = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         crossbow = GameObject.FindGameObjectWithTag("Crossbow").transform;
-        if (crossbow.localScale.y == -1)
-        {
-            direction = -1;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += crossbow.right * arrowSpeed * direction * Time.deltaTime;
+        transform.position += crossbow.right * arrowSpeed * Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
